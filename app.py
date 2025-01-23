f3c3a2bb6865fd214045deb32dc44af6ae95cd45
ev1025 <eg2874@naver.com>
2025-01-23 21:30:49 +0900

    체인릿설정

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from langchain_community.llms import HuggingFaceEndpoint
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+import chainlit as cl
+from dotenv import load_dotenv
+
+# .env 파일에서 Hugging Face API 키 로드
+load_dotenv()
+HF_API_KEY = os.getenv("HF_API_KEY")
+model_id = 'Envy1025/math_teachear_mingle'
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+            """
+            You are an AI assistant that provides helpful answers to user queries.
+            """),
+        ("user", "{question}\n"),
+    ]
+)
+
+@cl.on_chat_start
+async def main():
+    # Hugging Face 엔드포인트 LLM 초기화
+    llm = HuggingFaceEndpoint(
+        repo_id=model_id, 
+        max_new_tokens=500,
+        temperature=0.5, 
+        token=HF_API_KEY
+    )
+
+    # LLM 체인 생성
+    llm_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True)
+
+    # 사용자 세션에 LLM 체인 저장
+    cl.user_session.set("llm_chain", llm_chain)
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    # 사용자 세션에서 LLM 체인 가져오기
+    llm_chain = cl.user_session.get("llm_chain")
+    
+    # 메시지에 대한 응답 생성
+    res = await llm_chain.acall(message.content, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    
+    # 응답 메시지 전송
+    await cl.Message(content=res["text"]).send()
---
+++ chainlit.md
@@ -0,0 +1,16 @@
+# Math teacher Mingle 💁 
+
+안녕하세요! 초등학교, 중학교 수학교사 Mingle입니다!
+저는 구글의 gemma2를 기반으로 파인 튜닝 되었습니다!
+
+**사용 모델**
+[unsloth/gemma-2-2b-bnb-4bit](https://huggingface.co/unsloth/gemma-2-2b-bnb-4bit)
+
+**사용 데이터**
+허깅페이스 : [beomi/KoAlpaca-v1.1a](https://huggingface.co/datasets/beomi/KoAlpaca-v1.1a)
+자체 제작 : [수학 문답 데이터](https://huggingface.co/datasets/Envy1025/mathdata)
+
+코드와 관련하여 궁금한 점이 있으시면 해당 링크를 확인하세요!
+
+[Github](https://github.com/ev1025/gemma_sprint)
+
