59d52e075751735345e1bbdc1e7965b473f322b9
ev1025 <eg2874@naver.com>
2025-01-24 01:49:03 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -1,48 +1,43 @@
 import os
-from langchain_community.llms import HuggingFaceEndpoint
-from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain_community.llms import LlamaCpp
+from langchain.prompts import ChatPromptTemplate
 import chainlit as cl
-from dotenv import load_dotenv
 
-# .env 파일에서 Hugging Face API 키 로드
-load_dotenv()
-HF_API_KEY = os.getenv("HF_API_KEY")
-model_id = 'Envy1025/math_teachear_mingle'
+# LlamaCpp 모델 로드
+llm = LlamaCpp(
+    model_path=os.path.join(os.path.dirname(__file__), 'unsloth.F16.gguf'),
+    n_threads=8
+)
 
+# 수학 문제 해결을 위한 프롬프트 설정
 prompt_template = ChatPromptTemplate.from_messages(
     [
         ("system",
             """
-            You are an AI assistant that provides helpful answers to user queries.
+            You are a highly skilled math teacher. Your primary goal is to solve math problems step by step with clear explanations. 
+            Avoid discussing unrelated topics. Focus solely on solving the given problem.
             """),
-        ("user", "{question}\n"),
+        ("user", "Here is a math problem: {question}\n"),
+        ("assistant", "Let's solve this step by step:\n"),
     ]
 )
-    
+
 @cl.on_chat_start
 async def main():
-    # Hugging Face 엔드포인트 LLM 초기화
-    llm = HuggingFaceEndpoint(
-        repo_id=model_id, 
-        max_new_tokens=500,
-        temperature=0.5, 
-        token=HF_API_KEY
-    )
-
-    # LLM 체인 생성
-    llm_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True) # 설명
+    # PromptTemplate를 이용한 LLMChain 생성
+    llm_chain = LLMChain(prompt=prompt_template, llm=llm, verbose=False)  # 디버깅 시 verbose=True 설정
 
     # 사용자 세션에 LLM 체인 저장
-    cl.user_session.set("llm_chain", llm_chain)
+    cl.user_session.set('llm_chain', llm_chain)
 
 @cl.on_message
 async def on_message(message: cl.Message):
     # 사용자 세션에서 LLM 체인 가져오기
     llm_chain = cl.user_session.get("llm_chain")
     
-    # 메시지에 대한 응답 생성
-    res = await llm_chain.acall(message.content, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    # 사용자 입력에 대한 응답 생성
+    res = await llm_chain.acall({"question": message.content}, callbacks=[cl.AsyncLangchainCallbackHandler()])
     
     # 응답 메시지 전송
     await cl.Message(content=res["text"]).send()
